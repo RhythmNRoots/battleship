7972785dd57c486928a0d3d82f75c2936640171f
Anna Sores <annaa.sores@gmail.com>
2025-01-29 16:49:08 +0100

    beginning_of_game function was modified, now it works in a basic level. Additional unused lines in comment section were removed.

--- battleship.py
+++ battleship.py
@@ -14,30 +14,22 @@ def adding_ships(grid, coordinates):
     grid[third_x][third_y] = 'x'
     return(grid) # returning the modified grid (list of lists)
 
-coordinate_test = input('Enter the coordinates of the ships:')
+def beginning_of_game():
+    """This function initializes the game, by adding the ships to the empty grid, to locations, defined by coordinates."""
+    empty_grid = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
+    ship_coordinates_user = [(0, 0), (0, 2), (2, 1)]
+    # later the coordinates should be a result of an input --> ship_coordinates_user = input('Enter the coordinates of the ships: ')
+    # and then the input should be converted to a list of tuples (containing integers)
+    user_grid = adding_ships(empty_grid, ship_coordinates_user)
+    draw_map(user_grid)
 
-#Turning input coordinates into tuples
-coordinate_test = tuple(coordinate_test)
-for element in coordinate_test:
-    element = tuple(int(element))
-    print(type(element))
-print(type(coordinate_test))
+# coordinate_test = input('Enter the coordinates of the ships:')
 
+# Turning input coordinates into tuples
+# coordinate_test = tuple(coordinate_test)
+# for element in coordinate_test:
+#     element = tuple(int(element))
+#     print(type(element))
+# print(type(coordinate_test))
 
-#def beginning_of_game():
-#    empty_grid = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
-#    coordinates_1 = input('ship positions:')
-    #converting coordinates_1 to tuples
-#    player_grid_1 = adding_ships(empty_grid, coordinates_1)
-#    draw_map(player_grid_1)
-
-
-#coordinate_1 = (1, 0)
-#player_grid_1 = adding_ships(empty_grid, coordinate_1)
-#draw_map(player_grid_1)
-#print()
-#coordinate_2 = (1, 1)
-#player_grid_2 = adding_ships(player_grid_1, coordinate_2)
-#draw_map(player_grid_2)
-
-#(first_x, first_y), (second_x, second_y), (third_x, third_y) = coordinates
+beginning_of_game()
